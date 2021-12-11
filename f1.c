fcced801671fdbcbc580558f0d1d5a8e6b888c7d
abdushukur-m <93641511+abdushukur-m@users.noreply.github.com>
2021-12-11 19:28:45 +0500

    Add files via upload

---
+++ f1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int n=7, k=4;
+    int c;
+    char ch='?';
+    c=(int)ch;
+    FILE* fp;
+    fp=fopen("f1.txt", "w+");
+
+    for (int i=0; i<n; i++){
+        for (int j=0; j<k; j++){
+            fputc(c, fp);
+        }
+        fprintf(fp, "\n");
+    }
+
+    rewind(fp);
+
+    while (1){
+        c=fgetc(fp);
+        if(feof(fp)){
+            break;
+        }
+        printf("%c", c);
+    }
+
+    fclose(fp);
+    remove("f1.txt");
+
+    return 0;
+}
